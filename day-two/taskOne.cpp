cc64d59b13f5d07f083151ae85464bfcd6879fc9
nobody <burningeyes@abv.bg>
2018-10-15 08:34:31 +0300

    task one reformed

--- day-two/taskOne.cpp
+++ day-two/taskOne.cpp
@@ -2,17 +2,23 @@
 
 using namespace std;
 
+void switchCase(char c){
+  if(c>64&&c<91){
+    c+=32;
+  }else if(c>96&&c<123){
+    c-=32;
+  }else{
+    cout<<"Wrong input!"<<endl;
+  }
+  cout<<(char)c;
+}
+
 int main(){
 
   char c;
   cin>>c;
 
-  if(c>64&&c<91){
-    c+=32;
-  }else if(c>96&&c<123){
-    c-=32;
-  }
-  cout<<(char)c;
+  switchCase(c);
 
   return 0;
 }
