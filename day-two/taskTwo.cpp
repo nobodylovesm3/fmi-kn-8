97c588f1c94bfe44e52751c0c3f0fa79f872a24f
nobody <burningeyes@abv.bg>
2018-10-15 08:48:49 +0300

    incomplete tasktwo added

---
+++ day-two/taskTwo.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+enum weekDays {
+  SUN,
+  MON,
+  TUE,
+  WED,
+  THURS,
+  FRI,
+  SAT
+};
+
+enum months {
+  JAN=1,
+  FEB=2,
+  MAR=3,
+  APR=4,
+  MAY=5,
+  JUN=6,
+  JUL=7,
+  AUG=8,
+  SEPT=9,
+  OCT=10,
+  NOV=11,
+  DEC=12
+};
+
+enum monthsNumberOfDays {
+  JAN_DAYS = 31,
+  FEB_DAYS = 28,
+  MAR_DAYS = 31,
+  APR_DAYS = 30,
+  MAY_DAYS = 31,
+  JUN_DAYS = 30,
+  JUL_DAYS = 31,
+  AUG_DAYS = 31,
+  SEP_DAYS = 30,
+  OCT_DAYS = 31,
+  NOV_DAYS = 30,
+  DEC_DAYS = 31
+};
+
+
+
+string weekDay(int years, short month, short date){
+  //got lazy
+}
+
+int main(){
+
+
+
+  return 0;
+}
